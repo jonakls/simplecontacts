158673f378bb4da6c2330be55dbf8de2ac4cc82c
jonakls <asmot54@hotmail.com>
2022-08-09 03:47:26 -0500

    feat: add attributes for contact user and add getters and setters

--- src/main/java/me/jonakls/simplecontacts/api/ContactUser.java
+++ src/main/java/me/jonakls/simplecontacts/api/ContactUser.java
@@ -6,14 +6,59 @@ import java.util.UUID;
 
 public class ContactUser {
 
-    private final String name;
     private final UUID uuid;
+    private final String nickname;
+    private String name;
+    private String secondName;
+    private String email;
+    private String phone;
+    private String password;
     private final Map<Integer, Contact> contactMap;
 
-    public ContactUser(String name) {
-        this.name = name;
+    public ContactUser(String nickname) {
+        this.nickname = nickname;
         this.uuid = UUID.randomUUID();
         this.contactMap = new HashMap<>();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getName() {
