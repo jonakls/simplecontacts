7e1c1766b0756542e452fe59af00cad3d9bc80c8
jonakls <asmot54@hotmail.com>
2022-08-15 13:28:31 -0500

    style: remove unnecessary comentary

--- src/main/java/me/jonakls/simplecontacts/SimpleContacts.java
+++ src/main/java/me/jonakls/simplecontacts/SimpleContacts.java
@@ -8,10 +8,8 @@ public class SimpleContacts {
     private static AccountsService accountsService;
 
     public static void main(String[] args) {
-        // Create accounts file and load accounts if it exists
         accountsService = new AccountsService();
 
-        // Create login form and show it
         new LoginForm(null);
     }
 
